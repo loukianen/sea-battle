cad68521396e1f9a0d8183416466ca6a9043126c
Konstantin Lukyanenok <loukianen@gmail.com>
2021-02-24 16:58:28 +0300

    experiment with Drag script

--- src/index.js
+++ src/index.js
@@ -2,4 +2,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import app from './app/app.jsx';
 
+// <script src="scripts/DragDropTouch.js"></script>
+
 app();
