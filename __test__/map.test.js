316cf76525bef4c72fcfd58bd77f6cc1b6f3599d
Konstantin Lukyanenok <loukianen@gmail.com>
2021-09-27 13:13:03 +0300

    add BattleMap and test

--- __test__/map.test.js
+++ __test__/map.test.js
@@ -1,4 +1,6 @@
 import BattleMap from '../src/app/bin/BattleMap';
+import Coords from '../src/app/bin/Coords';
+import Square from '../src/app/bin/Square';
 
 describe('buttle map', () => {
   test.each([3, 5, 7, 10])('is creating right size %s', (mapSize) => {
@@ -12,20 +14,40 @@ describe('buttle map', () => {
   });
 
   const squaresCheckingData = [
-    [3],
-    [5],
-    [7],
-    [10],
+    [3, { f: '101', s: '202', t: '303' }],
+    [5, { f: '101', s: '303', t: '505' }],
+    [7, { f: '101', s: '404', t: '707' }],
+    [10, { f: '101', s: '606', t: '1010' }],
   ];
 
-  test.each(squaresCheckingData)('has right sqeares (size %s)', (mapSize, gaugeSquares) => {
+  test.each(squaresCheckingData)('squares checking %s', (mapSize, ids) => {
     const map = new BattleMap(mapSize);
-    const { squareIds, squares } = map.getSquares();
+    const firstPosition = 0;
+    const middlePosition = Math.round((mapSize - 1) / 2);
+    const lastPosition = mapSize - 1;
 
-    const firstSqueare = squares[101];
+    expect(map.getSquareById(ids.f)).toEqual(new Square(ids.f, [firstPosition, firstPosition]));
+    expect(map.getSquareById(ids.s)).toEqual(new Square(ids.s, [middlePosition, middlePosition]));
+    expect(map.getSquareById(ids.t)).toEqual(new Square(ids.t, [lastPosition, lastPosition]));
+  });
 
-    expect(size).toBe(mapSize);
-    expect(squareIds.length).toBe(size ** 2);
-    expect(squareIds).toEqual(mapSquareIds);
+  test('successful getters test', () => {
+    const map = new BattleMap(5);
+    const controlSquare = new Square('203', [2, 1]);
+    expect(map.getSquareByCoodrs(new Coords([2, 1]))).toEqual(controlSquare);
+    expect(map.getSquareById('203')).toEqual(controlSquare);
+    expect(map.getSquares()).toMatchObject({
+      size: expect.any(Number),
+      squareIds: expect.any(Array),
+      squares: expect.any(Object),
+    });
+  });
+
+  test('unsuccessful getters test', () => {
+    const map = new BattleMap();
+    expect(() => map.getSquareByCoodrs([2, 1]))
+      .toThrowError(/^Argument of function getSquareByCoodrs should be instance of Coords class$/);
+    expect(() => map.getSquareById(203))
+      .toThrowError(/^Argument of function getSquareById should be string$/);
   });
 });
--- src/app/bin/BattleMap.js
+++ src/app/bin/BattleMap.js
@@ -1,8 +1,26 @@
+import Coords from './Coords';
+import Square from './Square';
+
 export default class BattleMap {
-  constructor(mapSize) {
+  constructor(mapSize = 10) {
     this.squareIds = [];
     this.squares = {};
     this.makeMap(mapSize);
+  }
+
+  getSquareByCoodrs(coords) {
+    if (coords instanceof Coords) {
+      const squareId = this.map[coords.getY()][coords.getX()];
+      return this.squares[squareId];
+    }
+    throw new Error('Argument of function getSquareByCoodrs should be instance of Coords class');
+  }
+
+  getSquareById(squareId) {
+    if (typeof squareId === 'string') {
+      return this.squares[squareId];
+    }
+    throw new Error('Argument of function getSquareById should be string');
   }
 
   getSquares() {
@@ -14,14 +32,31 @@ export default class BattleMap {
   }
 
   makeMap(size) {
-    this.map = Array(size).fill([]).map((arr, indexY) => {
+    this.map = Array(size).fill(101).map((startId, indexY) => {
+      const res = [];
       for (let indexX = 0; indexX < size; indexX += 1) {
-        const id = String(101 + 100 * indexY + indexX);
-        arr.push(id);
+        const id = String(startId + 100 * indexY + indexX);
+        res.push(id);
         this.squareIds.push(id);
-        this.squares[id] = { id, type: 'clear', coords: { x: indexX, y: indexY } };
+        this.squares[id] = new Square(id, [indexX, indexY]);
       }
-      return arr;
+      return res;
     });
+  }
+
+  markSquareAsClear(id) {
+    this.squares[id].setType('clear');
+  }
+
+  markSquareAsFired(id) {
+    this.squares[id].setType('fired');
+  }
+
+  markSquareAsShip(id, shipId) {
+    this.squares[id].setType('ship', shipId);
+  }
+
+  markSquareAsKilledShip(id, shipId) {
+    this.squares[id].setType('killed-ship', shipId);
   }
 }
